bbc7ba285141096ee50f56f1a391dffe70d1d2c2
lehuutuong0803 <86358406+lehuutuong0803@users.noreply.github.com>
2021-06-28 13:46:21 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace WebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Company()
+        {
+            return View();
+        }
+        public ActionResult Furniture()
+        {
+            return View();
+        }
+    }
+}
